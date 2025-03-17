669f15173193a8c740c261e66101611a33b756af
TheSkylerDev <kesch.inno14@gmail.com>
2025-03-17 16:02:27 +0100

    Update ezsalcalc.kt

--- v0.1.0/ezsalcalc.kt
+++ v0.1.0/ezsalcalc.kt
@@ -6,9 +6,10 @@ data class Workers{
         Person("John", 2130),
         Person("Pjotr", 2132),
         Person("Ivan", 2131),
-        Person("Alex", 2133)
+        Person("Alex", 2133),
         Person("Hans", 2134)
     );
+    val sortedWorkers = workers.sort(workers, compareBy{it.id});
 }
 
 class SalaryCalculator(var: Int){
@@ -17,4 +18,7 @@ class SalaryCalculator(var: Int){
 
 fun main(){
     //main logic here
+    for (worker in workers){
+        println("Salary for ${worker.name} is ${worker.salary}");
+    };
 }
