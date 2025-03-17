548253c2af69b18622760878ec74212e6b3b4817
TheSkylerDev <kesch.inno14@gmail.com>
2025-03-17 13:53:29 +0100

    Build general layout template for ezsalcalc.kt

---
+++ v0.1.0/ezsalcalc.kt
@@ -0,0 +1,20 @@
+//EzSalaryCalculator: An app for calculating salaries without tax
+
+data class Workers{
+    //Worker Class logic here
+    val workers = listOf(
+        Person("John", 2130),
+        Person("Pjotr", 2132),
+        Person("Ivan", 2131),
+        Person("Alex", 2133)
+        Person("Hans", 2134)
+    );
+}
+
+class SalaryCalculator(var: Int){
+    //salary calculator logic here
+}
+
+fun main(){
+    //main logic here
+}
