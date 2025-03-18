acaca40b31c2725185d546bc22f44195dfaabd11
TheSkylerDev <kesch.inno14@gmail.com>
2025-03-18 13:27:01 +0100

    Finished ezsalcalc.py

--- v0.1.0/ezsalcalc.py
+++ v0.1.0/ezsalcalc.py
@@ -1,19 +1,39 @@
-#python version of ezsalcalc :p
-class workers:
-    def __init__(self, name, salary):
+#python version of ezsalcalc :p this is a minimal viuable product
+class Workers: #class for workers
+    def __init__(self, name, id): # constructor
         self.name = name
         self.id = id
+    
+    @staticmethod
+    def workerList(): #list of existing workers
+        workers = [workers("John", 1),
+                    workers("Pjotr", 2),
+                    workers("Hans", 3),
+                    workers("Johan", 4),
+                    workers("Carlito", 5),
+                    workers("Miguel", 6)
+                    ]
 
-class grossPay:
+class GrossPay:
     def __init__(self, hours, rate):
         self.hours = hours
         self.rate = rate
         self.gross = hours * rate
+    
+    def grossPay(self):
+        return self.gross
 
 def main():
     print("Welcome toi EZ Salary Calculator")
     print("Please enter the following information:")
-    input("Enter the worker's name: ") = workers.name
-    input("Enter the worker's ID: ") = workers.id
-    input("Enter the number of hours worked: ") = grossPay.hours
-    input("Enter the hourly rate: ") = grossPay.rate
+    name = input("Enter the worker's name: ")
+    id = int(input("Enter the worker's ID: "))
+    worker = Workers(name, id)
+    hours = float(input("Enter the number of hours worked: "))
+    rate = float(input("Enter the hourly rate: "))
+    gross = GrossPay(hours, rate)
+    result = f"Worker's name: {worker.name}\nWorker's ID: {worker.id}\nGross pay: {gross.gross}"
+    print(result)
+
+if __name__ == "__main__":
+    main()
