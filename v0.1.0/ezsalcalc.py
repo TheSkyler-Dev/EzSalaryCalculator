f36a0ba2af5dd5d9a6b9d2d2d43ac78c7dbd738f
TheSkylerDev <kesch.inno14@gmail.com>
2025-03-18 11:49:21 +0100

    Add ezsalcalc.py

---
+++ v0.1.0/ezsalcalc.py
@@ -0,0 +1,19 @@
+#python version of ezsalcalc :p
+class workers:
+    def __init__(self, name, salary):
+        self.name = name
+        self.id = id
+
+class grossPay:
+    def __init__(self, hours, rate):
+        self.hours = hours
+        self.rate = rate
+        self.gross = hours * rate
+
+def main():
+    print("Welcome toi EZ Salary Calculator")
+    print("Please enter the following information:")
+    input("Enter the worker's name: ") = workers.name
+    input("Enter the worker's ID: ") = workers.id
+    input("Enter the number of hours worked: ") = grossPay.hours
+    input("Enter the hourly rate: ") = grossPay.rate
